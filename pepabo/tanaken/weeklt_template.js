e40e0f1cd6245ece5d39510fe8893e7b045ba69f
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-07-19 22:44:50 +0900

    Create weeklt_template.js

---
+++ pepabo/tanaken/weeklt_template.js
@@ -0,0 +1,52 @@
+(function() {
+  const thisWeekStartDay = moment().day(0).format("YYYY/MM/DD")
+  const lastWeekStartDay = moment().day(0).add(-7, "d").format("YYYY/MM/DD")
+  const nextWeekStartDay = moment().day(0).add(7, "d").format("YYYY/MM/DD")
+  const d = moment().day(0).add(7, "d")
+  
+  // ここで返してるものがページのタイトルと本文になる
+  return `
+weekly tanaken ${thisWeekStartDay}~
+[left arrow.icon] [weekly tanaken ${lastWeekStartDay}~][blank.icon*21][weekly tanaken ${nextWeekStartDay}~] [right arrow.icon]
+#weekly_report #weekly_tanaken
+
+[hr.icon]
+[** ${d.add(-1, "d").format("MM/DD(ddd)")}]
+[* 天気] そと= カラダ= こころ=
+[* ログ]
+
+[hr.icon]
+[** ${d.add(-1, "d").format("MM/DD(ddd)")}]
+[* 天気] そと= カラダ= こころ=
+[* ログ]
+
+[hr.icon]
+[** ${d.add(-1, "d").format("MM/DD(ddd)")}]
+[* 天気] そと= カラダ= こころ=
+[* ログ]
+
+[hr.icon]
+[** ${d.add(-1, "d").format("MM/DD(ddd)")}]
+[* 天気] そと= カラダ= こころ=
+[* ログ]
+
+[hr.icon]
+[** ${d.add(-1, "d").format("MM/DD(ddd)")}]
+[* 天気] そと= カラダ= こころ=
+[* ログ]
+
+[hr.icon]
+[** ${d.add(-1, "d").format("MM/DD(ddd)")}]
+[* 天気] そと= カラダ= こころ=
+[* ログ]
+
+[hr.icon]
+[** ${d.add(-1, "d").format("MM/DD(ddd)")}]
+[* 天気] そと= カラダ= こころ=
+[* ログ]
+
+[hr.icon]
+[** 先週の宿題]
+
+`
+})()
